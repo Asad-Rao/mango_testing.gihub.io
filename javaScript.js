a351def3287178cf4b44928549f23e3490c674d0
AsadKhalilRao <mrao.bee18seecs@seecs.edu.pk>
2023-06-21 14:28:35 +0500

    commit

---
+++ javaScript.js
@@ -0,0 +1,27 @@
+// // window.addEventListener('resize', function() {
+// //     var screenWidth = window.innerWidth;
+// //     var textElement = document.getElementById('order_text_div');
+  
+// //     // Set the width of the text element based on the screen width
+// //     textElement.style.width = screenWidth * 0.4 + 'px';
+// //   });
+  
+// //   // Initial call to set the initial width based on the screen size
+// //   window.dispatchEvent(new Event('resize'));
+
+
+
+// window.addEventListener('resize', function() {
+//     var backgroundImage = document.querySelector('.background_image');
+//     var containerWidth = backgroundImage.parentNode.offsetWidth;
+//     var containerHeight = backgroundImage.parentNode.offsetHeight;
+//     var imageAspectRatio = 1080 / 1080; // Adjust this value based on the aspect ratio of your background image
+
+//     if (containerWidth / containerHeight > imageAspectRatio) {
+//         backgroundImage.style.width = '100%';
+//         backgroundImage.style.height = 'auto';
+//     } else {
+//         backgroundImage.style.width = 'auto';
+//         backgroundImage.style.height = '100%';
+//     }
+// });
